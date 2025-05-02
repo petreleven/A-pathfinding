a7e13b26667370c50136c055a47434e6b7d3a11e
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-05-02 08:59:28 +0300

    Delete builds directory

--- builds/imgui/CMakeFiles/imgui_impl_glfw.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libimgui_impl_glfw.a"
-)
--- builds/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francis/a/raylib
-# Build directory: /home/francis/a/builds/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
